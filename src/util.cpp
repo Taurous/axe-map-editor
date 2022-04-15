60c72cc0798525fda50c484aede940381d9d1f53
Aksel Huff <akselhuff@gmail.com>
2022-04-15 09:09:53 -0700

    Builds/runs on windows

--- src/util.cpp
+++ src/util.cpp
@@ -3,6 +3,12 @@
 #include <sstream>
 #include <stdlib.h>
 
+#ifdef WIN32
+	#define WIN32_LEAN_AND_MEAN
+	#include <windows.h>
+	#include <shlobj.h>
+#endif
+
 vec2i getScreenSize()
 {
 	return vec2i{al_get_display_width(al_get_current_display()), al_get_display_height(al_get_current_display())};
@@ -43,8 +49,20 @@ ALLEGRO_DISPLAY *createDisplay(std::string title, int width, int height, int fla
 
 std::string getHomeDir()
 {
-    const char* env_var = nullptr;
+#ifdef WIN32
+    CHAR my_docs[MAX_PATH];
+	HRESULT res = SHGetFolderPath(NULL, CSIDL_PERSONAL, NULL, SHGFP_TYPE_CURRENT, my_docs);
+
+	if (res != S_OK)
+	{
+		std::cerr << "Error getting My Documents path, code: " << res << "\n";
+		return "";
+	}
+	else return std::string(my_docs);
+#elif defined(LINUX)
+	const char* env_var = nullptr;
     env_var = secure_getenv("HOME");
 
     return std::string(env_var);
+#endif
 }
